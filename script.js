2f04b93875d192321e245c23bd899a4b913fdb09
TheLegion007 <manisksing@gmail.com>
2021-12-22 02:34:19 +0530

    :heavy_plus_sign: implements function to get task data

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
+//const taskContainer = document.getElementsByClassName("task_Container");
+const taskContainer = document.querySelector(".task_Container");
+console.log(taskContainer);
+
 const saveChanges = () => {
+   // get task data
   const taskData = {
     id: `${Date.now()}`,
     imageurl: document.getElementById("imageURL").value,
@@ -6,6 +11,25 @@ const saveChanges = () => {
     tasktype: document.getElementById("taskType").value,
     taskdescription: document.getElementById("taskDescription").value
   };
-  console.log(taskData);
+  //console.log(taskData);
   //onclick() in .html file to trigger this function//
-}
+  const newCard = `
+  <div id= {taskData.id} class="col-md-6 col-lg-4">
+    <div class="card">
+      <div class="card-header d-flex justify-content-end gap-2">
+        <button type="button" class="btn btn-outline-success"><i class="fas fa-pencil-alt"></i></button>
+        <button type="button" class="btn btn-outline-danger"><i class="fas fa-trash-alt"></i></button>
+      </div>
+       <img class="card-img-top" src= ${taskData.imageurl} alt="...">
+      <div class="card-body">
+        <h5 class="card-title text-primary">${taskData.tasktitle}</h5>
+        <p class="card-text">${taskData.tasktype}</p>
+        <a href="#" class="btn btn-primary">${taskData.taskdescription}</a>
+      </div>
+    </div>
+  </div>
+  `;
+  // insertAdjacentHTML is a inbuilt method
+  taskContainer.insertAdjacentHTML("beforeend", newCard);
+
+};
